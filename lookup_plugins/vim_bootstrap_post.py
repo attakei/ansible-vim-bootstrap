0d226a36fa303a5367a7cc2720359fd73c040b15
Kazuya Takei <myself@attakei.net>
2021-03-14 02:36:35 +0900

    Enable other sections (theme and frameworks)

--- lookup_plugins/vim_bootstrap_post.py
+++ lookup_plugins/vim_bootstrap_post.py
@@ -1,15 +1,21 @@
 from ansible.plugins.lookup import LookupBase
 
 
-QUERY_TERMS = (
-    "langs",
+QUERY_TERM_RULES = (
+    (list, "langs"),
+    (list, "frameworks"),
+    (str, "theme"),
+    (str, "editor"),
 )
 
 
 class LookupModule(LookupBase):
     def run(self, terms, **kwargs):
         query = []
-        for idx, key in enumerate(QUERY_TERMS):
+        for idx, rule in enumerate(QUERY_TERM_RULES):
+            if rule[0] == str:
+                query.append((rule[1], terms[idx]))
+                continue
             for val in terms[idx]:
-                query.append((key, val))
+                query.append((rule[1], val))
         return query
